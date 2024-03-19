4c2fa213569131327c342ae472f9f935a1be1c4e
SarahIrons <sarahirons11@gmail.com>
2024-03-19 15:32:44 -0400

    Add project files.

---
+++ ChallengeAssignment547/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using ChallengeAssignment547.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallengeAssignment547.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //Design a simple ASP.NET Core web app that displays the actual server time in the browser.
+        public IActionResult Index()
+
+        {
+            ViewData["TimeString"] = DateTime.Now.ToString("f");//the "f" designation notes that the time will be displayed with full day of week spelled out and then with time in US English format and AM/PM noted.
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
